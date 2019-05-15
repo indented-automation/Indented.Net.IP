a6ad8d993f3217b0897e203ebcf465d99203e519
Chris Dent <chris@indented.co.uk>
2019-05-15 12:17:09 +0100

    Adds test file header

--- Indented.Net.IP/test/public/ConvertTo-Mask.tests.ps1
+++ Indented.Net.IP/test/public/ConvertTo-Mask.tests.ps1
@@ -1,3 +1,19 @@
+#region:TestFileHeader
+param (
+    [Boolean]$UseExisting
+)
+
+if (-not $UseExisting) {
+    $moduleBase = $psscriptroot.Substring(0, $psscriptroot.IndexOf("\test"))
+    $stubBase = Resolve-Path (Join-Path $moduleBase "test*\stub\*")
+    if ($null -ne $stubBase) {
+        $stubBase | Import-Module -Force
+    }
+
+    Import-Module $moduleBase -Force
+}
+#endregion
+
 InModuleScope Indented.Net.IP {
     Describe 'ConvertTo-Mask' {
         It 'Returns an IPAddress' {
